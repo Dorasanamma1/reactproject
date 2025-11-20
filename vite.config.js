eb146634c4bd0e63b85ea3aca8ae5ef8c882c209
Dorasanamma1 <dorasanammaanki@gmail.com>
2025-11-20 14:52:22 +0530

    mydev

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/e-commerce-project/',   // <-- MUST BE YOUR REPO NAME
+  base: '/reactproject/'
 })
-
